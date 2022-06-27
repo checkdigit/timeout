20b3dbfcbd8775a311c2b0081bb11fb048548480
Carl Ansley <carl.ansley@gmail.com>
2022-06-27 14:38:31 -0400

    Update to node 16 (#8)

--- src/timeout.ts
+++ src/timeout.ts
@@ -1,7 +1,7 @@
 // timeout.ts
 
 /*
- * Copyright (c) 2021 Check Digit, LLC
+ * Copyright (c) 2021-2022 Check Digit, LLC
  *
  * This code is licensed under the MIT license (see LICENSE.txt for details).
  */
@@ -11,10 +11,10 @@ export interface TimeoutOptions {
 }
 
 const MINIMUM_TIMEOUT = 1; // 1ms
-const MAXIMUM_TIMEOUT = 900000; // 15 minutes
+const MAXIMUM_TIMEOUT = 900_000; // 15 minutes
 
 const DEFAULT_OPTIONS: Required<TimeoutOptions> = {
-  timeout: 60000, // 1 minute
+  timeout: 60_000, // 1 minute
 };
 
 export class TimeoutError extends Error {
@@ -36,18 +36,17 @@ export default async function <Type>(
   if (timeout < MINIMUM_TIMEOUT || timeout > MAXIMUM_TIMEOUT) {
     // Node's built-in setTimeout will default the delay to 1ms if the delay is larger than 2147483647ms or less than 1ms.
     // Instead, we error if the argument is invalid.
-    throw RangeError(`The timeout must be >= ${MINIMUM_TIMEOUT} and <= ${MAXIMUM_TIMEOUT}`);
+    throw new RangeError(`The timeout must be >= ${MINIMUM_TIMEOUT} and <= ${MAXIMUM_TIMEOUT}`);
   }
 
-  let handle: number | undefined;
+  let handle: number | undefined | NodeJS.Timeout;
   try {
     return (await Promise.race([
       promise,
       new Promise((_, reject) => {
-        // this setTimeout is currently mis-typed (sometimes...) as NodeJS.Timeout, when in fact it returns a number.
-        handle = (setTimeout(() => {
+        handle = setTimeout(() => {
           reject(new TimeoutError(timeout));
-        }, timeout) as unknown) as number;
+        }, timeout);
       }),
     ])) as Type;
   } finally {
